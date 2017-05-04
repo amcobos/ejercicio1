6db7f064014a98ffb4640c9e2c317ea07580bd23
amcobos <cobos_cobos@hotmail.com>
2017-05-04 14:39:10 +0200

    Add files via upload

--- ejercicio1.py
+++ ejercicio1.py
@@ -27,40 +27,8 @@ def array_to_dict(arr, keys):
 
     return d
 
-def array_to_dict2(arr, keys):
-    d = dict()
-    d12 = dict()
-    d2 = dict()
-    d3 = dict()
-    for e in arr:
-        if e.has_key(keys[0]) and e.has_key(keys[1]) and e.has_key(keys[2]):
 
 
 
-
-            d.setdefault(e[keys[0]], {}).setdefault(e[keys[1]], {}).setdefault(e[keys[2]], [e]).append(e)
-
-        elif not e.has_key(keys[0]):
-
-            print keys[0] + " no encontrada"
-
-            return array_to_dict(arr,['Hotel','Room','Board'])
-
-        elif not  e.has_key(keys[1]):
-
-            print keys[1] + " no encontrada"
-
-            return array_to_dict(arr,['Hotel','Room','Board'])
-
-        else:
-
-            print keys[2] + " no encontrada"
-
-            return array_to_dict(arr,['Hotel','Room','Board'])
-
-    return d
-
-
-
-result = array_to_dict2(arr,['Hotel','Room','Board'])
+result = array_to_dict(arr,['Hotel','Room','Board'])
 print(result)
